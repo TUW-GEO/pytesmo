db34da15c02bf1bc9751fc102c43b96ca59f58ef
Christoph Paulik <cpaulik@gmail.com>
2013-11-07 13:15:16 +0100

    updated to autocompile the filters extension

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 from distutils.core import setup
+from distutils.extension import Extension
+import numpy as np
+from distutils.command.sdist import sdist as _sdist
+
+
+class sdist(_sdist):
+    def run(self):
+        # Make sure the compiled Cython files in the distribution are up-to-date
+        from Cython.Build import cythonize
+        cythonize(['pytesmo/time_series/mycythonmodule.pyx'])
+        _sdist.run(self)
+cmdclass = {}
+cmdclass['sdist'] = sdist
+
+
+ext_modules = [
+    Extension("pytesmo.time_series.filters", [ "pytesmo/time_series/filters.c" ],
+              include_dirs=[np.get_include()]),
+]
 
 setup(
     name='pytesmo',
@@ -7,6 +26,8 @@ setup(
     author_email='Christoph.Paulik@geo.tuwien.ac.at',
     packages=['pytesmo', 'pytesmo.timedate', 'pytesmo.grid', 'pytesmo.io', 'pytesmo.io.sat', 'pytesmo.io.ismn',
               'pytesmo.time_series', 'pytesmo.timedate'],
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
     scripts=['bin/plot_ASCAT_data.py', 'bin/plot_ISMN_data.py', 'bin/compare_ISMN_ASCAT.py'],
     url='http://rs.geo.tuwien.ac.at/validation_tool/pytesmo/',
     license='LICENSE.txt',
