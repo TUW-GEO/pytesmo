01c068848807c05aa05de5c292a18bd2fa0940b1
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2015-07-09 14:41:48 +0200

    new temporal matching lead to new values in validation tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -26,7 +26,6 @@
 # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 '''
 Tests for the validation framework
 Created on Mon Jul  6 12:49:07 2015
@@ -50,10 +49,10 @@ from pytesmo.validation_framework.validation import Validation
 
 
 class DataPreparation(object):
-
     """
     Class for preparing the data before validation.
     """
+
     @staticmethod
     def prep_reference(reference):
         """
@@ -72,7 +71,10 @@ class DataPreparation(object):
         return reference
 
     @staticmethod
-    def prep_other(other, other_name, mask_snow=80, mask_frozen=80, mask_ssf=[0, 1]):
+    def prep_other(other, other_name,
+                   mask_snow=80,
+                   mask_frozen=80,
+                   mask_ssf=[0, 1]):
         """
         Static method used to prepare the other datasets (ASCAT).
 
@@ -118,13 +120,11 @@ def test_ascat_ismn_validation():
     """
     Test processing framework with some ISMN and ASCAT sample data
     """
-    ascat_data_folder = os.path.join(os.path.dirname(__file__),
-                                     'test-data', 'sat',
-                                     'ascat', 'netcdf', '55R22')
+    ascat_data_folder = os.path.join(os.path.dirname(__file__), 'test-data',
+                                     'sat', 'ascat', 'netcdf', '55R22')
 
-    ascat_grid_folder = os.path.join(os.path.dirname(__file__),
-                                     'test-data', 'sat',
-                                     'ascat', 'netcdf', 'grid')
+    ascat_grid_folder = os.path.join(os.path.dirname(__file__), 'test-data',
+                                     'sat', 'ascat', 'netcdf', 'grid')
 
     ascat_reader = AscatH25_SSM(ascat_data_folder, ascat_grid_folder)
     ascat_reader.read_bulk = True
@@ -132,14 +132,16 @@ def test_ascat_ismn_validation():
 
     # Initialize ISMN reader
 
-    ismn_data_folder = os.path.join(os.path.dirname(__file__),
-                                    'test-data', 'ismn', 'multinetwork', 'header_values')
+    ismn_data_folder = os.path.join(os.path.dirname(__file__), 'test-data',
+                                    'ismn', 'multinetwork', 'header_values')
     ismn_reader = ISMN_Interface(ismn_data_folder)
 
     jobs = []
 
     ids = ismn_reader.get_dataset_ids(
-        variable='soil moisture', min_depth=0, max_depth=0.1)
+        variable='soil moisture',
+        min_depth=0,
+        max_depth=0.1)
     for idx in ids:
         metadata = ismn_reader.metadata[idx]
         jobs.append((idx, metadata['longitude'], metadata['latitude']))
@@ -153,21 +155,33 @@ def test_ascat_ismn_validation():
 
     # Create the validation object.
 
-    datasets = {'ISMN': {'class': ismn_reader, 'columns': ['soil moisture'],
-                         'type': 'reference', 'args': [], 'kwargs': {}},
-                'ASCAT': {'class': ascat_reader, 'columns': ['sm'], 'type': 'other',
-                          'args': [], 'kwargs': {}, 'grids_compatible': False,
-                          'use_lut': False, 'lut_max_dist': 30000}
-                }
+    datasets = {
+        'ISMN': {
+            'class': ismn_reader, 'columns': [
+                'soil moisture'
+            ], 'type': 'reference', 'args': [], 'kwargs': {}
+        },
+        'ASCAT': {
+            'class': ascat_reader, 'columns': [
+                'sm'
+            ], 'type': 'other', 'args': [], 'kwargs': {}, 'grids_compatible':
+            False, 'use_lut': False, 'lut_max_dist': 30000
+        }
+    }
 
     period = [datetime(2007, 1, 1), datetime(2014, 12, 31)]
 
-    process = Validation(datasets=datasets, data_prep=DataPreparation(),
-                         temporal_matcher=temporal_matchers.BasicTemporalMatching(
-        window=1 / 24.0, reverse=True),
-        scaling='lin_cdf_match', scale_to_other=True,
+    process = Validation(
+        datasets=datasets,
+        data_prep=DataPreparation(),
+        temporal_matcher=temporal_matchers.BasicTemporalMatching(
+            window=1 / 24.0,
+            reverse=True),
+        scaling='lin_cdf_match',
+        scale_to_other=True,
         metrics_calculator=metrics_calculators.BasicMetrics(),
-        period=period, cell_based_jobs=False)
+        period=period,
+        cell_based_jobs=False)
 
     for job in jobs:
         results = process.calc(job)
@@ -176,14 +190,16 @@ def test_ascat_ismn_validation():
     results_fname = os.path.join(
         save_path, 'ISMN.soil moisture_with_ASCAT.sm.nc')
 
-    vars_should = [u'n_obs', u'tau', u'gpi', u'RMSD', u'lon',
-                   u'p_tau', u'BIAS', u'p_rho', u'rho', u'lat', u'R', u'p_R']
-    n_obs_should = [360,  385, 1644, 1881, 1927,  479,  139,  251]
-    rho_should = np.array([0.54618734,  0.71739876,  0.62089276,  0.53246528,  0.30299741,
-                           0.69647062,  0.84350693,  0.73913699], dtype=np.float32)
+    vars_should = [u'n_obs', u'tau', u'gpi', u'RMSD', u'lon', u'p_tau',
+                   u'BIAS', u'p_rho', u'rho', u'lat', u'R', u'p_R']
+    n_obs_should = [360, 385, 1644, 1881, 1927, 479, 140, 251]
+    rho_should = np.array([0.54618734, 0.71739876, 0.62089276, 0.53246528,
+                           0.30299741, 0.69647062, 0.840593, 0.73913699],
+                          dtype=np.float32)
 
-    rmsd_should = np.array([11.53626347,   7.54565048,  17.45193481,  21.19371414,
-                            14.24668026,  14.27493,  13.1489048,  12.59192371], dtype=np.float32)
+    rmsd_should = np.array([11.53626347, 7.54565048, 17.45193481, 21.19371414,
+                            14.24668026, 14.27493, 13.173215, 12.59192371],
+                           dtype=np.float32)
     with nc.Dataset(results_fname) as results:
         assert sorted(results.variables.keys()) == sorted(vars_should)
         assert sorted(results.variables['n_obs'][:].tolist()) == sorted(
