ee7698b8370288627340c2a1ba897d1219e9f7aa
Teije van der Horst <tvanderhorst@vandersat.com>
2019-04-29 17:57:47 +0200

    Added minimum amount option
    now even better

--- pytesmo/time_series/anomaly.py
+++ pytesmo/time_series/anomaly.py
@@ -95,7 +95,9 @@ def calc_climatology(Ser,
                      wraparound=False,
                      respect_leap_years=False,
                      interpolate_leapday=False,
-                     fillna=True):
+                     fillna=True,
+                     min_obs_orig=1,
+                     min_obs_clim=1):
     '''
     Calculates the climatology of a data set.
 
@@ -134,8 +136,15 @@ def calc_climatology(Ser,
 
     fillna: boolean, optional
         If set, then the moving average used for the calculation of the
-        climatology will be filled at the nan-values (for at least 1 valid
-        observation in every `moving_avg_orig`-day window
+        climatology will be filled at the nan-values
+
+    min_obs_orig: int
+        Minimum observations required to give a valid output in the first
+        moving average applied on the input series
+
+    min_obs_clim: int
+        Minimum observations required to give a valid output in the second
+        moving average applied on the calculated climatology
 
     Returns
     -------
@@ -147,7 +156,7 @@ def calc_climatology(Ser,
     if timespan is not None:
         Ser = Ser.truncate(before=timespan[0], after=timespan[1])
 
-    Ser = moving_average(Ser, window_size=moving_avg_orig, fillna=fillna)
+    Ser = moving_average(Ser, window_size=moving_avg_orig, fillna=fillna, min_obs=min_obs_orig)
 
     Ser = pd.DataFrame(Ser)
 
@@ -163,10 +172,9 @@ def calc_climatology(Ser,
 
     if respect_leap_years:
         doys = doy(month, day, year)
-        doy_leap = 366
     else:
         doys = doy(month, day)
-        doy_leap = 60
+
 
     Ser['doy'] = doys
 
@@ -179,9 +187,9 @@ def calc_climatology(Ser,
                          index=clim.index.values)
 
     if interpolate_leapday and not respect_leap_years:
-        clim_ser[doy_leap] = np.mean((clim_ser[59], clim_ser[61]))
+        clim_ser[60] = np.mean((clim_ser[59], clim_ser[61]))
     elif interpolate_leapday and respect_leap_years:
-        clim_ser[doy_leap] = np.mean((clim_ser[365], clim_ser[1]))
+        clim_ser[366] = np.mean((clim_ser[365], clim_ser[1]))
 
     if wraparound:
         index_old = clim_ser.index.copy()
@@ -195,11 +203,11 @@ def calc_climatology(Ser,
                               clim_ser,
                               right_mirror])
 
-        clim_ser = moving_average(clim_ser, window_size=moving_avg_clim, fillna=fillna)
+        clim_ser = moving_average(clim_ser, window_size=moving_avg_clim, fillna=fillna, min_obs=min_obs_clim)
         clim_ser = clim_ser.iloc[moving_avg_clim:-moving_avg_clim]
         clim_ser.index = index_old
     else:
-        clim_ser = moving_average(clim_ser, window_size=moving_avg_clim, fillna=fillna)
+        clim_ser = moving_average(clim_ser, window_size=moving_avg_clim, fillna=fillna, min_obs=min_obs_clim)
 
     clim_ser = clim_ser.reindex(np.arange(366) + 1)
     clim_ser = clim_ser.fillna(fill)
