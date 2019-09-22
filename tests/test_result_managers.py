e50eea5238e9fb74e253c8cc2c2e279d5d080dcc
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2019-09-22 21:51:36 +0200

    Remove new results manager tests

--- tests/test_result_managers.py
+++ tests/test_result_managers.py
@@ -1,25 +1,44 @@
-# -*- coding: utf-8 -*-
+# Copyright (c) 2016,Vienna University of Technology,
+# Department of Geodesy and Geoinformation
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#   * Neither the name of the Vienna University of Technology, Department of
+#     Geodesy and Geoinformation nor the names of its contributors may be used
+#     to endorse or promote products derived from this software without specific
+#     prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL VIENNA UNIVERSITY OF TECHNOLOGY, DEPARTMENT OF
+# GEODESY AND GEOINFORMATION BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 '''
-Module description
+Tests for the results managers.
 '''
-# TODO:
-#   (+)
-#---------
-# NOTES:
-#   -
 
 import tempfile
 import numpy as np
 import os
 import netCDF4
-import shutil
 
 from pytesmo.validation_framework.results_manager import netcdf_results_manager
-from pytesmo.validation_framework.new_results_manager import *
 
 
-def test_netcdf_result_manager_new_n2():
+def test_netcdf_result_manager_n2():
 
     tst_results = {
         (('DS1', 'x'), ('DS3', 'y')): {
@@ -63,11 +82,7 @@ def test_netcdf_result_manager_new_n2():
             'p_R': np.array([0.], dtype=np.float32)}}
 
     tempdir = tempfile.mkdtemp()
-    manag = NcResultsManager(tempdir)
-    manag.add(tst_results)
-    var_attr = {'R':{'name': 'Pearson'}}
-    glob_attr = {'test_attribute': 'test'}
-    manag.to_netcdf(var_attr=var_attr, glob_attr=glob_attr)
+    netcdf_results_manager(tst_results, tempdir)
     assert sorted(os.listdir(tempdir)) == sorted(['DS1.x_with_DS3.x.nc',
                                                   'DS1.x_with_DS3.y.nc',
                                                   'DS1.x_with_DS2.y.nc'])
@@ -76,11 +91,9 @@ def test_netcdf_result_manager_new_n2():
     with netCDF4.Dataset(os.path.join(tempdir, 'DS1.x_with_DS3.x.nc')) as ds:
         assert ds.variables['lon'][:] == np.array([4])
         assert ds.variables['n_obs'][:] == np.array([1000])
-        assert(ds.variables['R'].getncattr('name') == 'Pearson')
-        assert(ds.getncattr('test_attribute') == 'test')
 
 
-def test_netcdf_result_manager_new_n3():
+def test_netcdf_result_manager_n3():
 
     tst_results = {
         (('DS1', 'x'), ('DS2', 'y'), ('DS3', 'x')): {
@@ -111,11 +124,7 @@ def test_netcdf_result_manager_new_n3():
             'p_R': np.array([0.], dtype=np.float32)}}
 
     tempdir = tempfile.mkdtemp()
-    manag = NcResultsManager(tempdir)
-    manag.add(tst_results)
-    var_attr = {'R':{'name': 'Pearson'}}
-    glob_attr = {'test_attribute': 'test'}
-    manag.to_netcdf(var_attr=var_attr, glob_attr=glob_attr)
+    netcdf_results_manager(tst_results, tempdir)
     assert sorted(os.listdir(tempdir)) == sorted(['DS1.x_with_DS2.y_with_DS3.x.nc',
                                                   'DS1.x_with_DS2.y_with_DS3.y.nc'])
 
@@ -123,5 +132,5 @@ def test_netcdf_result_manager_new_n3():
     with netCDF4.Dataset(os.path.join(tempdir, 'DS1.x_with_DS2.y_with_DS3.x.nc')) as ds:
         assert ds.variables['lon'][:] == np.array([4])
         assert ds.variables['n_obs'][:] == np.array([1000])
-        assert(ds.variables['R'].getncattr('name') == 'Pearson')
-        assert(ds.getncattr('test_attribute') == 'test')
+
+
