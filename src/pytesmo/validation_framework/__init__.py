e4a7417fb84f4f7dfad6d4e7d33f0f21547374ab
Samuel Scherrer <samuel.scherrer@posteo.de>
2021-04-07 14:27:12 +0200

    removed imports from validation_framework init

--- src/pytesmo/validation_framework/__init__.py
+++
@@ -1,9 +0,0 @@
-from pytesmo.validation_framework.validation import Validation
-from pytesmo.validation_framework.metric_calculators import (
-    PairwiseIntercomparisonMetrics,
-    TripletMetrics,
-)
-from pytesmo.validation_framework.temporal_matchers import (
-    BasicTemporalMatching,
-    make_combined_temporal_matcher,
-)
