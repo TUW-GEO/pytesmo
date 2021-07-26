da976f0c2dc48b5ff20f7c02044cd4bcd00b6dd9
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2021-07-26 12:40:59 +0200

    Update deprecated decorator and pep8

--- src/pytesmo/timedate/julian.py
+++ src/pytesmo/timedate/julian.py
@@ -5,23 +5,36 @@ To install cadati, run 'pip install cadati'
 This module will be removed in future versions of pytesmo
 """
 
-import numpy as np
 import pandas as pd
 import datetime as dt
 import pytz
 
 import warnings
-warnings.warn("pytesmo.timdate.julian has moved to the cadati pip package "
-              "Import from pytesmo will not work in future versions of the package",
-              DeprecationWarning)
 
 from cadati.check_date import is_leap_year
-from cadati.jd_date import julday, caldat, julian2date, julian2datetime, \
-    julian2num, num2julian
+from cadati.jd_date import (
+    julday,
+    caldat,
+    julian2date,
+    julian2datetime,
+    julian2num,
+    num2julian,
+)
 from cadati.conv_doy import doy
 from pytesmo.utils import deprecated
 
-@deprecated
+# deprecated calls:
+__all__ = ['is_leap_year', 'julday', 'caldat', 'julian2datetime',
+           'julian2num', 'num2julian', 'doy']
+
+warnings.warn(
+    "pytesmo.timdate.julian has moved to the cadati pip package "
+    "Import from pytesmo will not work in future versions of the package",
+    DeprecationWarning,
+)
+
+
+@deprecated()
 def julian2doy(j, consider_nonleap_years=True):
     """
     Calendar date from julian date.
@@ -37,9 +50,12 @@ def julian2doy(j, consider_nonleap_years=True):
     doy : numpy.ndarray or int32
         Day of year.
     """
-    return julian2date(j, return_doy=True, doy_leap_year=not consider_nonleap_years)[-1]
+    return julian2date(
+        j, return_doy=True, doy_leap_year=not consider_nonleap_years
+    )[-1]
 
-@deprecated
+
+@deprecated()
 def julian2datetimeindex(j, tz=pytz.UTC):
     """
     Converting Julian days to datetimeindex.
@@ -56,7 +72,11 @@ def julian2datetimeindex(j, tz=pytz.UTC):
     """
     year, month, day, hour, minute, second, microsecond = julian2date(j)
 
-    return pd.DatetimeIndex([dt.datetime(y, m, d, h, mi, s, ms, tz)
-                             for y, m, d, h, mi, s, ms in
-                             zip(year, month, day, hour, minute,
-                                 second, microsecond)])
+    return pd.DatetimeIndex(
+        [
+            dt.datetime(y, m, d, h, mi, s, ms, tz)
+            for y, m, d, h, mi, s, ms in zip(
+                year, month, day, hour, minute, second, microsecond
+            )
+        ]
+    )
