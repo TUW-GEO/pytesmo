9ffa54e73776e8e183ee89186de4c3f88d948cc3
pstradio <77664155+pstradio@users.noreply.github.com>
2022-05-13 13:36:59 +0200

    Added timestamp adapter (#268)
    * Added timestamp adapter
    
    * Documentation
    
    * Corrected default
    
    * yapf formatted
    
    * Improved functionality
    
    * Separated replace index and name declaration
    
    * handled empty dataframe
    
    * Updated changelog
    
    * Changed parameter name
    
    * Handle invalid in adapter
    
    * simplified empty dataframe cases

--- tests/test_validation_framework/test_adapters.py
+++ tests/test_validation_framework/test_adapters.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.pytesmo.validation_framework.adapters import TimestampAdapter
 """
 Test for the adapters.
 """
@@ -11,11 +12,9 @@ import os
 import pandas as pd
 import warnings
 
-from pytesmo.validation_framework.adapters import (
-    MaskingAdapter,
-    AdvancedMaskingAdapter,
-    ColumnCombineAdapter
-)
+from pytesmo.validation_framework.adapters import (MaskingAdapter,
+                                                   AdvancedMaskingAdapter,
+                                                   ColumnCombineAdapter)
 from pytesmo.validation_framework.adapters import SelfMaskingAdapter
 from pytesmo.validation_framework.adapters import AnomalyAdapter
 from pytesmo.validation_framework.adapters import AnomalyClimAdapter
@@ -36,23 +35,21 @@ def test_masking_adapter():
         nptest.assert_almost_equal(
             data_masked["x"].values,
             np.concatenate(
-                [np.ones((10), dtype=bool), np.zeros((10), dtype=bool)]
-            ),
+                [np.ones((10), dtype=bool),
+                 np.zeros((10), dtype=bool)]),
         )
         nptest.assert_almost_equal(
             data_masked2["x"].values,
             np.concatenate(
-                [np.ones((10), dtype=bool), np.zeros((10), dtype=bool)]
-            ),
+                [np.ones((10), dtype=bool),
+                 np.zeros((10), dtype=bool)]),
         )
 
         if col is None:
-            nptest.assert_almost_equal(
-                data_masked["y"].values, np.ones((20), dtype=bool)
-            )
-            nptest.assert_almost_equal(
-                data_masked2["y"].values, np.ones((20), dtype=bool)
-            )
+            nptest.assert_almost_equal(data_masked["y"].values,
+                                       np.ones((20), dtype=bool))
+            nptest.assert_almost_equal(data_masked2["y"].values,
+                                       np.ones((20), dtype=bool))
 
 
 def test_self_masking_adapter():
@@ -145,14 +142,15 @@ def test_anomaly_clim_adapter_one_column():
 
 
 def test_adapters_custom_fct_name():
+
     def assert_all_read_fcts(reader):
-        assert(np.all(reader.read() == reader.read_ts()))
-        assert(np.all(reader.read() == reader.alias_read()))
+        assert (np.all(reader.read() == reader.read_ts()))
+        assert (np.all(reader.read() == reader.alias_read()))
 
     base = TestDataset("", n=20)
     assert_all_read_fcts(base)
-    sma = SelfMaskingAdapter(base, '>=', 5, column_name='y',
-                             read_name='alias_read')
+    sma = SelfMaskingAdapter(
+        base, '>=', 5, column_name='y', read_name='alias_read')
     assert_all_read_fcts(sma)
     smanom = AnomalyAdapter(sma, read_name='alias_read')
     assert_all_read_fcts(smanom)
@@ -221,23 +219,21 @@ def test_adapters_with_ascat():
 
 
 class TestTimezoneReader(object):
+
     def read(self, *args, **kwargs):
         data = np.arange(5.0)
         data[3] = np.nan
         return pd.DataFrame(
             {"data": data},
             index=pd.date_range(
-                datetime(2007, 1, 1, 0), "2007-01-05", freq="D", tz="UTC"
-            ),
+                datetime(2007, 1, 1, 0), "2007-01-05", freq="D", tz="UTC"),
         )
 
     def read_ts(self, *args, **kwargs):
         return self.read(*args, **kwargs)
 
 
-@pytest.mark.filterwarnings(
-    "ignore:Dropping timezone information:UserWarning"
-)
+@pytest.mark.filterwarnings("ignore:Dropping timezone information:UserWarning")
 def test_timezone_removal():
     tz_reader = TestTimezoneReader()
 
@@ -258,8 +254,11 @@ def test_column_comb_adapter():
     ds = TestDataset("", n=20)
     orig = ds.read()
     ds_adapted = ColumnCombineAdapter(
-        ds, func=pd.DataFrame.mean, columns=["x", "y"],
-        func_kwargs={'skipna': True}, new_name='xy_mean')
+        ds,
+        func=pd.DataFrame.mean,
+        columns=["x", "y"],
+        func_kwargs={'skipna': True},
+        new_name='xy_mean')
     ds_mean1 = ds_adapted.read_ts()
     ds_mean2 = ds_adapted.read()
 
@@ -268,3 +267,172 @@ def test_column_comb_adapter():
         nptest.assert_equal(ds_mean["y"].values, orig["y"].values)
         nptest.assert_equal(ds_mean["xy_mean"].values,
                             (ds_mean["x"] + ds_mean["y"]) / 2.)
+
+
+def test_timestamp_adapter():
+    ds = TestDataset("", n=20)
+
+    # Simple case
+    # ================
+
+    index = np.arange('2005-02', '2005-03', dtype='datetime64[D]')
+    sm_var = np.random.randn(*index.shape)
+    time_offset_field = np.random.normal(
+        loc=1000.0, scale=1.0, size=index.shape).astype(int)
+
+    def _read():
+        return pd.DataFrame(
+            data=np.array([sm_var, time_offset_field]).transpose(),
+            columns=["sm", "offset"],
+            index=index)
+
+    setattr(ds, "read", _read)
+    origin = ds.read().drop(columns="offset")
+
+    adapted_ds = TimestampAdapter(
+        ds, time_offset_fields="offset", time_units="s")
+    adapted = adapted_ds.read()
+
+    # Date should be unchanges as we are using a ~1000 sec offset
+    assert (origin.index.date == adapted.index.date).all()
+    # The offset is expressed in seconds
+    assert origin.index[0] + np.timedelta64(time_offset_field[0],
+                                            "s") == adapted.index[0]
+    # The dataframe is integral
+    assert (origin.columns == adapted.columns).all()
+
+    adapted_ds = TimestampAdapter(
+        ds, time_offset_fields="offset", time_units="m")
+    adapted = adapted_ds.read()
+
+    # The offset is expressed in minutes
+    assert origin.index[0] + np.timedelta64(time_offset_field[0],
+                                            "m") == adapted.index[0]
+
+    # This time we do not drop the columns
+    origin = ds.read()
+    adapted_ds = TimestampAdapter(
+        ds, time_offset_fields="offset", time_units="s", drop_original=False)
+    adapted = adapted_ds.read()
+
+    # The dataframe is integral
+    assert (origin.columns == adapted.columns).all()
+
+    # test use of new column
+    # -----------------------
+    adapted_ds = TimestampAdapter(
+        ds,
+        time_offset_fields="offset",
+        time_units="s",
+        replace_index=False,
+        output_field="exact_timestamp")
+    adapted = adapted_ds.read()
+    assert (adapted.columns == ["sm", "exact_timestamp"]).all()
+
+    # test NaNs in offset and generic time
+    # -----------------------
+    index = np.arange('2005-02', '2005-03', dtype='datetime64[D]')
+    index[4] = np.datetime64("NaT")
+    sm_var = np.random.randn(*index.shape)
+    time_offset_field = np.random.normal(
+        loc=1000.0, scale=1.0, size=index.shape)
+    time_offset_field[7] = np.nan
+
+    def _read_nans():
+        return pd.DataFrame(
+            data=np.array([sm_var, time_offset_field]).transpose(),
+            columns=["sm", "offset"],
+            index=index)
+
+    setattr(ds, "read", _read_nans)
+    origin = ds.read()
+
+    adapted_ds = TimestampAdapter(
+        ds, time_offset_fields="offset", time_units="s")
+    adapted = adapted_ds.read()
+
+    # One index (NaT) value should be dropped
+    assert len(adapted.index) == len(origin.index) - 1
+    # The Nan offset should be interpreted as 0
+    assert adapted.index[6] == datetime(2005, 2, 8, 0, 0)
+
+    # test all NaNs in dataframe
+    # -----------------------
+    index = np.arange('2005-02', '2005-03', dtype='datetime64[D]')
+    index[:] = np.datetime64("NaT")
+    sm_var = np.random.randn(*index.shape)
+    time_offset_field = np.random.normal(
+        loc=1000.0, scale=1.0, size=index.shape)
+    time_offset_field[7] = np.nan
+
+    def _read_all_nans():
+        return pd.DataFrame(
+            data=np.array([sm_var, time_offset_field]).transpose(),
+            columns=["sm", "offset"],
+            index=index)
+
+    setattr(ds, "read", _read_all_nans)
+    origin = ds.read()
+
+    adapted_ds = TimestampAdapter(
+        ds, time_offset_fields="offset", time_units="s")
+    adapted = adapted_ds.read()
+
+    # The original should be returned
+    pd.testing.assert_frame_equal(origin, adapted)
+
+    # test empty Dataframe
+    # -----------------------
+
+    def _read_empty():
+        return pd.DataFrame(columns=["sm", "offset"],)
+
+    setattr(ds, "read", _read_empty)
+    origin = ds.read()
+
+    adapted_ds = TimestampAdapter(
+        ds, time_offset_fields="offset", time_units="s")
+    adapted = adapted_ds.read()
+
+    # The original should be returned
+    assert adapted.empty
+    assert (adapted.columns == ["sm", "offset"]).all()
+
+    # Complex case
+    # ================
+    base_time = np.arange(100, 200)
+    index = base_time.copy()
+    sm_var = np.random.randn(*index.shape)
+    time_offset_field_min = np.random.normal(
+        loc=40.0, scale=1.0, size=index.shape).astype(int)
+    time_offset_field_sec = np.random.normal(
+        loc=1000.0, scale=1.0, size=index.shape).astype(int)
+
+    def _read_complex():
+        return pd.DataFrame(
+            data=np.array([
+                sm_var, base_time, time_offset_field_min, time_offset_field_sec
+            ]).transpose(),
+            columns=["sm", "base_time", "offset_min", "offset_sec"],
+            index=index)
+
+    setattr(ds, "read", _read_complex)
+    origin = ds.read()
+
+    adapted_ds = TimestampAdapter(
+        ds,
+        time_offset_fields=["offset_min", "offset_sec"],
+        time_units=["m", "s"],
+        base_time_field="base_time",
+        base_time_reference="2005-02-01")
+
+    adapted = adapted_ds.read()
+
+    should_be = origin.apply(
+        lambda row: np.datetime64("2005-02-01") + np.timedelta64(
+            int(row["base_time"]), "D") + np.timedelta64(
+                int(row["offset_min"]), "m") + np.timedelta64(
+                    int(row["offset_sec"]), "s"),
+        axis=1).values
+
+    assert (adapted.index.values == should_be).all()
