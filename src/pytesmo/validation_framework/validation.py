d67e3e3d33c776964c5c3f8ae71169809c96d921
s-scherrer <samuel.scherrer@geo.tuwien.ac.at>
2023-03-02 14:32:17 +0100

    respect max_dist in validation (#291)
    * respect max_dist in validation
    
    * test for max dist

--- src/pytesmo/validation_framework/validation.py
+++ src/pytesmo/validation_framework/validation.py
@@ -245,7 +245,6 @@ class Validation(object):
             f"'handle_errors' must be one of {error_handling_options}"
         )
 
-
         if len(args) > 0:
             gpis, lons, lats, args = args_to_iterable(
                 gpis, lons, lats, *args, n=3
@@ -263,7 +262,7 @@ class Validation(object):
                     )
                 except Exception as e:
                     raise eh.DataManagerError(
-                        "Getting the data for gpi {gpi_info} failed!")
+                        f"Getting the data for gpi {gpi_info} failed!")
 
                 # if no data is available continue with the next gpi
                 if len(df_dict) == 0:
